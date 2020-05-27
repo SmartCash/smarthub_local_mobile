ed9317f3e632199ee23b7d9ac953bed392ef737a
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-05-27 15:24:47 -0300

    app

---
+++ App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import useCachedResources from './hooks/useCachedResources';
+
+import Navigator from './navigation/Navigator';
+
+export default function App(props) {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <Navigator/>
+    );
+  }
+}
+
