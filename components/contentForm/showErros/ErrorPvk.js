27c4bd1e7b7a76f5483a77b77b418c6b84e68931
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-06-19 17:47:16 -0300

    style error

--- components/contentForm/showErros/ErrorPvk.js
+++ components/contentForm/showErros/ErrorPvk.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { View, Text} from "react-native";
-import Styles from "../styles";
+import { View, Text, StyleSheet } from "react-native";
 
- export default function ErrorPvk({errors}) {
+export default function ErrorPvk({ errors }) {
   return (
     <View>
       {errors.privateKey && (
-        <Text className="error-message" style={Styles.msgError}>
+        <Text className="error-message" style={styles.msgError}>
           {errors.privateKey.message}
         </Text>
       )}
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  msgError: {
+    marginTop: 0,
+    color: "#ff1111",
+  },
+});
