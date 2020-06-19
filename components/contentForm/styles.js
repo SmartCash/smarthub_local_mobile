5743096824b718aa373e5686a018786d1e1cc495
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-06-19 17:46:39 -0300

    styles code

--- components/contentForm/styles.js
+++ components/contentForm/styles.js
@@ -1,9 +1,31 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 export default styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f1f1f1",
+  },
+  card: {
+    marginTop: 10,
+    marginLeft: 12,
+    marginRight: 12,
+    shadowColor: "#f4b517",
+    shadowOffset: { width: -2, height: 3 },
+    shadowOpacity: 0.7,
+    shadowRadius: 3,
+    backgroundColor: "#fff",
+    padding: 10,
+    elevation:5
+  },
+  contentCard: {
+    marginBottom: 15,
+    backgroundColor: "#fff",
+    padding: 10,
+    shadowColor:Platform.OS ==="ios" ? "#f4b517" : "#f1f1",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.5,
+    width: 325,
+    elevation:3
   },
   input: {
     padding: 10,
@@ -20,48 +42,41 @@ export default styles = StyleSheet.create({
     backgroundColor: "#fff",
     marginRight: 10,
   },
-  card: {
-    marginTop: 10,
-    marginLeft: 12,
-    marginRight: 12,
-    shadowColor: "black",
-    shadowOffset: { width: -1, height: 2 },
-    shadowOpacity: 0.26,
-    shadowRadius: 5,
-    backgroundColor: "#fff",
-    padding: 10,
-  },
-  contentCard: {
-    marginBottom: 20,
-    backgroundColor: "#f1f1f2",
-    padding: 10,
-    shadowColor: "black",
-    shadowOffset: { width: -1, height: 10 },
-    shadowOpacity: 0.09,
-    width: 325,
-  },
   text: {
     shadowOffset: { width: -1, height: 2 },
     shadowOpacity: 0.26,
     shadowRadius: 5,
     padding: 5,
-    backgroundColor: "#f1f1f1",
+    color: "rgba(0,0,0,0.6)",
+    fontSize: 15,
+    lineHeight: 20,
   },
   textBalance: {
     shadowRadius: 5,
     padding: 5,
-    backgroundColor: "#f1f1f2",
+    backgroundColor: "#f1f1f1"
+  },
+  textTransactions:{
+    shadowColor: "#f4b517",
+    shadowOffset: { width: -2, height: 3 },
+    shadowOpacity: 0.7,
+    shadowRadius: 0.2,
+    padding: 5
   },
   button: {
-    paddingHorizontal: 10,
+    paddingHorizontal: 0,
     paddingVertical: 5,
+    marginVertical:5,
+  },
+  buttonSend:{
+    alignContent:"space-around",
+    elevation:5,
   },
   balance: {
     flexDirection: "row",
     justifyContent: "space-around",
     flex: 1,
-    marginTop: -15,
-    marginBottom: -10,
+    marginBottom: -15,
   },
   msgError: {
     color: "#ff1111",
@@ -70,11 +85,6 @@ export default styles = StyleSheet.create({
   },
   marginError: {
     marginTop: 10,
-  },
-  text: {
-    color: "rgba(0,0,0,0.6)",
-    fontSize: 15,
-    lineHeight: 20,
   },
   Balance: {
     marginLeft: 10,
@@ -85,6 +95,9 @@ export default styles = StyleSheet.create({
     marginTop: "2%",
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  fee:{
+    alignContent:"center"
   },
   amount: {
     flexDirection: "row",
@@ -104,5 +117,4 @@ export default styles = StyleSheet.create({
   Button: {
     borderRadius: 50,
   },
-
 });
